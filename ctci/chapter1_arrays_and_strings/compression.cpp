a22164079c21c2e28f9e2474d45241ff9938daeb
jkim393 <jkim393@ucr.edu>
2019-07-15 07:59:47 -0700

    compression

--- ctci/chapter1_arrays_and_strings/compression.cpp
+++ ctci/chapter1_arrays_and_strings/compression.cpp
@@ -2,16 +2,27 @@
 
 using namespace std;
 
+string compression(string word){
+	string result = "";
+	int count = 0;
+	if (word.length() == 1){
+		return 
+	}
+	for (int i=0;i<word.length();i++){
+		count++;
+		if (word[i] != word[i+1] || i+1 >= word.length()) {
+			result += word[i] + count;
+			count = 0;
+		}
+	}
+	return ( result.length() < word.length() ? result : word );
+}
+
 int main(){
 	string word = "";
 	getline(cin, word);
 	
-	if ( isPermutation2(word) ){
-		cout << "Permutation";
-	}
-	else {
-		cout << "Not Permutation";
-	}
+	cout << compression(word);
 
 	return 0;
 }

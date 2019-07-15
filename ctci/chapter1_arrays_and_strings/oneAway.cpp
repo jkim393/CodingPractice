b420cee1cc62b94d5b55a26833d91b013348b1c9
jkim393 <jkim393@ucr.edu>
2019-07-15 06:02:50 -0700

    one away done

--- ctci/chapter1_arrays_and_strings/oneAway.cpp
+++ ctci/chapter1_arrays_and_strings/oneAway.cpp
@@ -1,16 +1,72 @@
+//2 strings given, check if replace/insert/remove one away
+//look at each method
+//replace -> same length
+//insert -> not same length -> compare char by char
 #include <iostream>
 
 using namespace std;
 
-int main(){
-	string word = "";
-	getline(cin, word);
-	
-	if ( isPermutation2(word) ){
-		cout << "Permutation";
+bool replace(string word, string word2){
+	int checker = 0;
+	for (int i=0; i<word.length(); i++){
+		if(word[i] != word2[i]){
+			if (checker == 0){
+				checker++;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool insert(string word, string word2){
+	int j, checker = 0;
+		for (int i=0; i<word.length(); i++){
+			if(word[j] != word2[i]){
+			
+				if (checker == 0){
+					checker++;
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				j++;
+			}
+		}
+	return true;
+}
+
+bool isOneaway(string word, string word2){
+	if (word.length() == word2.length() ){
+		return replace(word, word2);
 	}
 	else {
-		cout << "Not Permutation";
+		if (word.length() < word2.length() ){
+			return insert(word, word2);
+		}
+		else{
+			return insert(word2, word);
+		}
+	}
+	return true;
+}
+
+
+int main(){
+	string word, word2 = "";
+	getline(cin, word);
+	getline(cin, word2);
+	
+	
+	if ( isOneaway(word, word2) ){
+		cout << "One away";
+	}
+	else {
+		cout << "Not one away";
 	}
 
 	return 0;

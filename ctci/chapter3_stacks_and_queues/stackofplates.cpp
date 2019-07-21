ee627cc0b91259eaaccbaad3bb5e3d281bf41f20
jkim393 <jkim393@ucr.edu>
2019-07-20 19:32:37 -0700

    stack of plates

---
+++ ctci/chapter3_stacks_and_queues/stackofplates.cpp
@@ -0,0 +1,120 @@
+//set of stacks
+
+//using array as list of stacks
+//array is also used for each stack
+
+#include <iostream>
+
+using namespace std;
+
+//array implementation of stack
+#define STACK_SIZE 5
+
+class MyStack{
+private:
+	int top; //keep track of index of top
+	int s[STACK_SIZE]; // actual stack
+
+public:
+	MyStack * next = nullptr;
+	MyStack(){
+		top = -1; // when first element added, ++
+	}
+
+	void push(int x){
+		top++;
+		s[top] = x;
+		cout << x << " is added\n";
+	}
+
+	void pop(){
+		cout << s[top] << " is popped\n";
+		s[top] = 0;
+		top--;
+	}
+	int peek(){
+		if(!isEmpty())
+			return s[top];
+		else
+			cout << "Stack Empty\n";
+			return 0;;
+	}
+	bool isEmpty(){
+		return (top==-1) ? true : false;
+	}
+	bool isFull(){
+		return (top==STACK_SIZE-1) ? true : false;
+	}
+};
+
+class SetOfStacks {
+private:
+	MyStack * root = new MyStack();
+public:
+	void push(int d){
+		//if the root stack is full or no stack exists
+		//create new stack
+		//set root to the new stack
+		if(root->isFull()){ 
+			MyStack * newStack = new MyStack();
+			newStack->next = root;
+			root = newStack;
+			cout << "new stack created\n";
+		}
+		//push into the current stack
+		root->push(d);
+	}
+	void pop(){
+		if(isEmpty()){
+			cout << "Set of Stacks is empty\n";
+			return;
+		}
+		if(root->isEmpty()){
+			MyStack * temp = root;
+			root = root->next;
+			delete[] temp;
+			cout << "stack removed\n";
+		}
+		root->pop();
+	}
+	int top(){
+		if(!isEmpty())
+			return root->peek();
+		else
+			cout << "Set of Stacks is empty\n";
+			return 0;;
+	}
+	bool isEmpty(){
+		if (root->next == nullptr && root->isEmpty())
+			return true;
+		else
+			return false;
+	}
+};
+
+int main(){
+
+	SetOfStacks s;
+	s.push(5);
+	s.push(5);
+	s.push(5);
+	s.push(5);
+	s.push(5);
+	s.push(5);
+	s.push(5);
+	s.push(5);
+
+	s.pop();
+	s.pop();
+	s.pop();
+	s.pop();
+	s.pop();
+	s.pop();
+	s.pop();
+	s.pop();
+	s.pop();
+	s.pop();
+	s.pop();
+
+	return 0;
+}

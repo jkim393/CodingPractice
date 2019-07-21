b52d0d0b13b9183aa38e598b9cb26ca61c2a4bd0
jkim393 <jkim393@ucr.edu>
2019-07-21 01:20:28 -0700

    animalshelter incomplete

---
+++ ctci/chapter3_stacks_and_queues/animalShelter.cpp
@@ -0,0 +1,42 @@
+//incomplete
+
+#include <iostream>
+#include <iterator>
+#include <list>
+
+using namespace std;
+
+struct Animal{
+	string name = "";
+	string type = "";
+	int id;
+	Animal(string name, string type, int id){
+		h
+	}
+};
+
+
+class AnimalQueue{
+private:
+	list<Animal> Dog;
+	list<Animal> Cat;
+
+public:
+	void enqueue(Animal animal){
+
+	}
+	void dequeueAny(){
+
+	}
+	void dequeueDog(){
+
+	}
+	void dequeueCat(){
+
+	}
+};
+
+int main(){
+
+	return 0;
+}

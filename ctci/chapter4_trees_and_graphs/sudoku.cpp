de0caa6eba2065c5bb914302c285a8ace3ec7fbf
jkim393 <jkim393@ucr.edu>
2019-07-24 07:13:01 -0700

    sudoku

---
+++ ctci/chapter4_trees_and_graphs/sudoku.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+bool row(vector<vector<int>> v, int r){
+	for(int i=0;i<9;i++){
+		
+	}
+}
+
+bool col(vector<vector<int>> v, int c){
+
+}
+
+bool square(vector<vector<int>> v, int sq){
+
+}
+
+void sudoku(vector<vector<int>> & v){
+
+}
+
+
+int main(){
+	vector<vector<int>> v (9, vector<int>(9,1));
+
+	v[][] = ;
+
+	for(int i=0;i<9;i++){
+		for(int j=0;j<9;j++){
+			cout << v[i][j] << " ";
+		}
+		cout << endl;
+	}
+}

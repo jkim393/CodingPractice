8dd893e4d35ab57eccc3c0fe21b00c66a9c8ed6f
jkim393 <jkim393@ucr.edu>
2019-08-06 21:01:27 -0700

    route using class

--- ctci/chapter4_trees_and_graphs/routebetweennodes.cpp
+++ ctci/chapter4_trees_and_graphs/routebetweennodes.cpp
@@ -5,79 +5,67 @@
 
 using namespace std;
 
-//adjacency list implementation
-
-// class Graph
-// {
-// public:
-// 	int V;	// number of vertices
-// 	vector<int> *adj;  //adjacency list 
-
-// 	Graph(int V);
-// 	void addEdge(int x, int y);
-// 	bool isRoute(int x, int y);
-// };
-
-// // Constructor
-// Graph::Graph(int V)
-// {
-// 	this->V=V;
-// 	this->adj = new vector<int>[V];
-// }
-
-// // add a directed edge from x to y
-// void Graph::addEdge(int x, int y){
-// 	adj[x].push_back(y);
-// }
-
-void addEdge(vector<int> adj, int u, int v){ //u points to v
-	adj[u].push_back(v);
-}
-
-bool isRoute(std::vector<int> adj, int x, int y){ //simple BFS
-	vector<bool> visited(6, false); //set all of them to false
-	queue<int> q; //nearest neighbor of the current node
-
-	q.push(x); //original node x
-
-	while(!q.empty()){ // while q is not empty
-		int current = q.front();
-		if(current == y){
-			return true;
-		}
-		q.pop();
-		visited[current] = true;
-		int neighbor_size = adj[current].size();
-		for (int i=0;i<neighbor_size;i++){
-			if(!visited[adj[current][i]])
-				q.push(adj[current][i]);
-		}
+class Graph{
+private:	
+	int V;
+	vector<int> * adj;
+public:
+	Graph(int v){
+		this->V = v;
+		this->adj = new vector<int>[V]; // create an array of vectors where the array is size V
 	}
-	return false;
-}
+
+	void addEdge(int u, int v){ //directed graph; u->v
+		adj[u].push_back(v);
+	}
+
+	bool isRoute(int x, int y){
+		std::vector<bool> visited(V);
+		queue<int> q;
+		q.push(x);
+
+		while(!q.empty()){
+			int current = q.front();
+			if (current == y){
+				return true;
+			}
+			visited[current] = true;
+			q.pop();
+
+			//enqueue all neighbors of current
+			int neighbor_size = this->adj[current].size();
+			for(int i=0;i<neighbor_size;i++){
+				if( ! visited[ adj[current][i] ] ) { //if the neighboring vertex hasn't been visited
+					q.push(adj[current][i]); // enqueue that vertex
+				}
+			}
+
+		}
+
+	}
+
+};
 
 int main(){
+	Graph g(6);
 
-	int V = 6;
-	vector<int> adj[V];
-	addEdge(adj, 5, 2);
-	addEdge(adj, 5, 0);
-	addEdge(adj, 4, 0);
-	addEdge(adj, 4, 1);
-	addEdge(adj, 2, 3);
-	addEdge(adj, 3, 1);
+	g.addEdge(5, 2);
+	g.addEdge(5, 0);
+	g.addEdge(4, 0);
+	g.addEdge(4, 1);
+	g.addEdge(2, 3);
+	g.addEdge(3, 1);
 
-	if(isRoute(adj, 5,4)) //false
+	if(g.isRoute(5,4)) //false
 		cout << "true\n";
 	else
 		cout << "false\n";
 
 
-	if(isRoute(adj, 5,1)) //true
+	if(g.isRoute(5,1)) //true
 		cout << "true\n";
 	else
 		cout << "false\n";
-
 
 	return 0;
 }
